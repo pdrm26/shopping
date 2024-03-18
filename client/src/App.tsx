a10b785e486b735768afc2ce7035ec6e80f101e7
pedram <peeeedii@gmail.com>
2024-03-18 11:44:29 +0330

    feat: add base router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Panel from "./pages/Panel";
+import CustomerList from "./components/panels/CustomerList";
 
 export default function App() {
     return (
-        <>
-            <Panel />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Panel />}>
+                    <Route path="customersList" element={<CustomerList />} />
+                </Route>
+                <Route path="/news" element={<h1>news</h1>} />
+                <Route path="/about" element={<h1>about</h1>} />
+                <Route path="*" element={<h1>NOT_FOUND</h1>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
